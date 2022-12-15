29096984b012d4aeeb96b62f7a76270b3d316baa
b63 <avink5760@gmail.com>
2022-12-15 04:11:57 -0600

    change to init.lua

---
+++ init.lua
@@ -0,0 +1,24 @@
+vim.cmd("let $CONFPATH=$XDG_CONFIG_HOME.'/nvim'")
+vim.opt_global.exrc = true
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.opt.guifont = "BlexMono Nerd Font Mono:h10"
+
+-- loading plugsin (using packer)
+require'plugins'
+
+-- Plugin Config: {{{1
+local config_terminal_plugin = function()
+    vim.g.floaterm_autoclose = 1
+
+    vim.g.floaterm_autoclose = 1
+    vim.g.floaterm_height = 0.4
+    vim.g.floaterm_wintype = 'split'
+    vim.keymap("n", "<leader>;", ":call OpenScratchTerm()<CR>")
+    vim.keymap("n", "<leader>:", ":call OpenScratchTermDir(expand('%:p:h'))<CR>")
+    vim.keymap("t", "<C-W>N", "<C-\\><C-N>")
+end
+
+require'mylua/initrc'
+--vim.cmd("colorscheme ayu")
---
+++ lua/plugins.lua
@@ -0,0 +1,64 @@
+--[[
+-- You must run this or `PackerSync` whenever you make changes to your plugin configuration
+-- Regenerate compiled loader file
+:PackerCompile
+
+-- Remove any disabled or unused plugins
+:PackerClean
+
+-- Clean, then install missing plugins
+:PackerInstall
+
+-- Clean, then update and install plugins
+-- supports the `--preview` flag as an optional first argument to preview updates
+:PackerUpdate
+
+-- Perform `PackerUpdate` and then `PackerCompile`
+-- supports the `--preview` flag as an optional first argument to preview updates
+:PackerSync
+
+-- Show list of installed plugins
+:PackerStatus
+
+-- Loads opt plugin immediately
+:PackerLoad completion-nvim ale
+]]--
+
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  -- https://github.com/wbthomason/packer.nvim
+
+  -- color scheme/theme
+  -- use 'morhetz/gruvbox'
+  -- use 'arcticicestudio/nord-vim'
+  -- use 'sainnhe/sonokai'
+  -- use 'ayu-theme/ayu-vim'
+  use 'tomasiser/vim-code-dark'
+
+  -- Intellisense engine
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/nvim-cmp'
+
+  -- for easy surround
+  use 'tpope/vim-surround'
+
+  -- telescope
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+
+  -- tree-sitting
+  use 'nvim-treesitter/nvim-treesitter'
+
+  -- terminals
+  use 'voldikss/vim-floaterm'
+
+  -- winhist plugin
+  use('~/.local/share/nvim/plugins/win-hist')
+end)
