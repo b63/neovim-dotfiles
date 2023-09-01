0292cc78f3abf292d5751ab163ad6d02e49caa4f
b63 <avink5760@gmail.com>
2023-09-01 14:05:59 -0500

    lsp/term config

--- init.lua
+++ init.lua
@@ -15,10 +15,12 @@ local config_terminal_plugin = function()
     vim.g.floaterm_autoclose = 1
     vim.g.floaterm_height = 0.4
     vim.g.floaterm_wintype = 'split'
-    vim.keymap("n", "<leader>;", ":call OpenScratchTerm()<CR>")
-    vim.keymap("n", "<leader>:", ":call OpenScratchTermDir(expand('%:p:h'))<CR>")
-    vim.keymap("t", "<C-W>N", "<C-\\><C-N>")
+    vim.keymap.set("n", "<leader>;", ":call OpenScratchTerm()<CR>")
+    vim.keymap.set("n", "<leader>:", ":call OpenScratchTermDir(expand('%:p:h'))<CR>")
+    vim.keymap.set("t", "<C-W>n", "<C-\\><C-N>")
+    vim.keymap.set("t", "<C-W>;", "<C-\\><C-N>:call OpenScratchTerm()<CR>")
 end
 
 require'mylua/initrc'
+config_terminal_plugin()
 --vim.cmd("colorscheme ayu")
